2e33e5ab6bb596d66965e80b0457b189128ae7b4
soo <gjstn0308@naver.com>
2025-12-15 22:19:01 +0900

    Update 출력의 분리  : View 반환 시, controller의 리턴 값을 void로 하고 uri를 파일명과 일치하면 해당 파일 매핑됨

--- spring-boot-fastcampus-namgung/ch1/src/main/java/com/fastcampus/ch1/YoilTeller.java
+++ spring-boot-fastcampus-namgung/ch1/src/main/java/com/fastcampus/ch1/YoilTeller.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,7 +14,7 @@ import java.util.Calendar;
 public class YoilTeller {
 
   @RequestMapping("/yoil")
-  public String main(int year, int month, int day, Model model) throws IOException {
+  public void main(int year, int month, int day, Model model) throws IOException {
 
     // 2. 처리
     Calendar cal = Calendar.getInstance();
@@ -29,7 +30,7 @@ public class YoilTeller {
     model.addAttribute("yoil", yoil);
 
 
-    return "yoil"; // yoil.html - view의 이름을 반환
+//    return "yoil"; // yoil.html - view의 이름을 반환
 
 
 
