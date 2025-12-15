a440aa8a769d43e6459d142e4780859490067dbb
soo <gjstn0308@naver.com>
2025-12-15 22:17:10 +0900

    Update 출력의 분리  : MVC 패턴, View 반환 시 , Controller 어노테이션 사용 필요 / @RestController : RESTful API 사용 시 사용 (FE/BE 분리)  -

--- spring-boot-fastcampus-namgung/ch1/src/main/java/com/fastcampus/ch1/YoilTeller.java
+++ spring-boot-fastcampus-namgung/ch1/src/main/java/com/fastcampus/ch1/YoilTeller.java
@@ -1,45 +1,38 @@
 package com.fastcampus.ch1;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Calendar;
-import java.util.Enumeration;
 
-@RestController
+@Controller
 public class YoilTeller {
 
   @RequestMapping("/yoil")
-  public void main(int year, int month, int day, HttpServletResponse response) throws IOException {
+  public String main(int year, int month, int day, Model model) throws IOException {
 
     // 2. 처리
     Calendar cal = Calendar.getInstance();
+    cal.clear();
     cal.set(year, month - 1, day);
 
     int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
     char yoil = "일월화수목금토".charAt(dayOfWeek - 1);
 
+    model.addAttribute("year", year);
+    model.addAttribute("month", month);
+    model.addAttribute("day", day);
+    model.addAttribute("yoil", yoil);
+
+
+    return "yoil"; // yoil.html - view의 이름을 반환
+
+
 
     // 3. 출력
-    response.setCharacterEncoding("utf-8");
-    PrintWriter out = response.getWriter();
-
-    out.println("<html >");
-    out.println("<head>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("year = " + year);
-    out.println("month = " + month);
-    out.println("day = " + day);
-    out.println("yoil = " + yoil);
-//        out.println( "오늘 날짜 : "+cal );
-    out.println("</body>");
-    out.println("</html>");
-
-//        return "yoil";
   }
 }
